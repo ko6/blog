734635347f1e62f8ceb143b669027affa5a0a105
koko <kokostudio@qq.com>
2016-12-21 21:14:24 +0800

    完善分类页面标签显示

--- frontend/views/site/category.php
+++ frontend/views/site/category.php
@@ -1,12 +1,14 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
 
 // $this->title = '1';
 // $this->description = isset($site_info['site_name'])?$site_info['site_name']:'My Yii Application';
 
- $this->registerMetaTag ( [  'name' => "description" ,  'content'=>"new description",],"description");
- $this->registerMetaTag ( [  'name' => "keywords" ,  'content'=>"new keywords",],'keywords');
+//这里设置分类页面的关键字和描述。
+ // $this->registerMetaTag ( [  'name' => "description" ,  'content'=>"new description",],"description");
+ // $this->registerMetaTag ( [  'name' => "keywords" ,  'content'=>"new keywords",],'keywords');
 // var_dump($post);
 
 if (isset($post)) {
@@ -32,7 +34,18 @@ if (isset($post)) {
             <div class="blog_list pull-left">
               <ul class="list-unstyled">
                 <li><i class="fa fa-calendar-o"></i><span><?=date("Y-m-d",$p['created_at'])?></span></li>
-                <li><a href="#"><i class="fa fa-tags"></i><span><?=$p['post_hits']?></span></a></li>
+                <li><i class="fa fa-tags"></i>
+                  <?php
+                 if(isset($p['post_tips']) && $tips = array_unique(explode("|",$p['post_tips']))){
+                     if(count($tips)>0){
+                         foreach($tips as $tip){
+                             echo "<a href=". Url::to('/t/'.$tip)." >$tip</a>";
+                             }
+                         }
+                     }
+
+                  ?>
+                </li>
               </ul>
             </div>
             <div class="b_left blog_list pull-right">
